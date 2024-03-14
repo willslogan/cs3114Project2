60b0d97bac5363d0a32f1d73f3c9650cef83649e
Jacob Fast <jacobfast@vt.edu>
2024-03-14 18:44:34 -0400

    Added Tests for SkipList, Point, and command Processor

--- src/Point.java
+++ src/Point.java
@@ -1,72 +1,129 @@
-
+/**
+ * @author Will Logan
+ * @version 1.0
+ */
 public class Point {
+    // the x coordinate of the point
     private int x;
+    // the y coordinate of the point
     private int y;
+    // the name of the point
     private String name;
-    
+
+    /**
+     * Creates an object with the values given in the parameters
+     * 
+     * @param x
+     *            x-coordinate of the point
+     * @param y
+     *            y-coordinate of the point
+     * @param name
+     *            name of the point
+     */
     public Point(int x, int y, String name) {
         this.x = x;
         this.y = y;
         this.name = name;
     }
-    
+
+
+    /**
+     * Get the x coordinate of a point
+     * 
+     * @return x
+     *         x-coordinate of the point
+     */
     public int getX() {
         return x;
     }
-    
+
+
+    /**
+     * Get the y coordinate of a point
+     * 
+     * @return y
+     *         y-coodinate of the point
+     */
     public int getY() {
         return y;
     }
-    
+
+
+    /**
+     * Get the name of a point
+     * 
+     * @return name
+     *         name of the point
+     */
     public String getName() {
         return name;
     }
-    
-    public boolean isValid(Point p) {
-        //TODO
-        return false;
-    }
-    
+
+
+    /**
+     * Checks if two points are equal. Points are considered equal if they have
+     * the same x and y coordinate
+     * 
+     * @param other
+     *            the point we are comparing with
+     * @return true if the points have the same x and y coordinate
+     */
     public boolean equals(Object other) {
-        if(this == other) {
+        // Check if they reference the same thing
+        if (this == other) {
             return true;
         }
-        
-        if(other == null) {
+        // Check if the other object is null
+        if (other == null) {
             return false;
         }
-        
-        //Ensure they are same clas
-        if(other.getClass() != Point.class) {
+
+        // Ensure they are same class
+        if (other.getClass() != Point.class) {
             return false;
         }
-        
+
+        // Cast other to a point object
         Point otherPoint = (Point)other;
-        
-        if(this.getX() == otherPoint.getX() && this.getX() == otherPoint.getY()) {
+        // If the x and y coordinate, of both points, are equal return true
+        if (this.getX() == otherPoint.getX() && this.getX() == otherPoint
+            .getY()) {
             return true;
         }
-        
+        // Otherwise return false
         return false;
     }
-    
+
+
+    /**
+     * Outputs a human readable string with information about the point which
+     * includes the x and y coordinate and its name
+     * 
+     * @return a human readable string containing information about the point
+     */
     public String toString() {
         return "(" + name + ", " + x + ", " + y + ")";
     }
 
-    public boolean isInvalid() {
-        if(getX() < 0) {
+
+    /**
+     * Check if the point has valid parameters
+     * 
+     * @return true if the points has valid parameters, false if not 
+     */
+    public boolean isValid() {
+        if (getX() < 0) {
             return false;
         }
-        if(getY() < 0) {
+        if (getY() < 0) {
             return false;
         }
-        
-        if(getY() > 1024) {
+
+        if (getY() > 1024) {
             return false;
         }
-        
-        if(getX() > 1024) {
+
+        if (getX() > 1024) {
             return false;
         }
         return true;
